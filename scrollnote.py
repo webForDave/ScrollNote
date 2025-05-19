610ab684d35d49827f3c31bfdeff73ff38620dc1
David Akinola <dakinola54@gmail.com>
2025-05-19 20:25:53 +0100

    Daily logging core engine

--- scrollnote.py
+++ scrollnote.py
@@ -9,7 +9,19 @@ class ScrollNote():
         self.date = (datetime.today().strftime("%Y-%m-%d"))+'.txt'
         self.filepath = Path(logs_path) / self.date
 
+    def save(self):
+        with open(self.filepath, "w", encoding="utf-8") as file:
+            file.write(self.content)
+
+    def load(self):
+        try:
+            with open(self.filepath, "r") as file:
+                self.content = file.read()
+                print(self.content)
+        except FileNotFoundError:
+            pass
+
     def __str__(self):
         return f"{self.content}, {self.date}, {self.filepath}"
 
-print(ScrollNote("Hello"))
+print(ScrollNote("This is the replace version of today's log").load()) 
